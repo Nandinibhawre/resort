504ea950aa94ec0189f98ef8b0ff27738c433dee
nandini bhawre <nandinibhawre24@gmailcom>
2026-02-17 17:32:07 +0530

    aa

--- src/main/java/com/elite/resort/Repository/ImageRepo.java
+++ src/main/java/com/elite/resort/Repository/ImageRepo.java
@@ -1,8 +1,9 @@
 package com.elite.resort.Repository;
 
-
 import com.elite.resort.Model.Image;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ImageRepo extends MongoRepository<Image, String> {
 }
