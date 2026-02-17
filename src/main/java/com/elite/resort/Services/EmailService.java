b9aa86723cab521f1feb4ab39d484d196dd897c8
nandini bhawre <nandinibhawre24@gmailcom>
2026-02-18 00:10:55 +0530

    a

--- src/main/java/com/elite/resort/Services/EmailService.java
+++ src/main/java/com/elite/resort/Services/EmailService.java
@@ -126,7 +126,7 @@ public class EmailService {
 
 
         webClient.post()
-                .uri("https://api.brevo.com/v3/smtp/email")
+                .uri("https://api.brevo.com/v3")
                 .header("api-key", apiKey)
                 .contentType(MediaType.APPLICATION_JSON)
                 .bodyValue(body)
