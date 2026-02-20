62479722622eb231ea44dab1aa75bdbec29730c5
nandini bhawre <nandinibhawre24@gmailcom>
2026-02-20 10:22:05 +0530

    n

--- src/main/java/com/elite/resort/ResortApplication.java
+++ src/main/java/com/elite/resort/ResortApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
-@EnableMongoAuditing
+
 public class ResortApplication {
 
 	public static void main(String[] args) {
