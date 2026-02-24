ed7dd9746783c2df33128dde8cd68499052451b4
nandini bhawre <nandinibhawre24@gmailcom>
2026-02-24 12:46:24 +0530

    add

--- src/main/java/com/elite/resort/Controller/AdminRoomController.java
+++ src/main/java/com/elite/resort/Controller/AdminRoomController.java
@@ -3,6 +3,7 @@ package com.elite.resort.Controller;
 import com.elite.resort.DTO.AdminBookingView;
 import com.elite.resort.Model.Room;
 
+import com.elite.resort.Repository.RoomRepo;
 import com.elite.resort.Services.AdminRoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -18,33 +19,38 @@ public class AdminRoomController {
 
     private final AdminRoomService adminRoomService;
 
+    //Adding rooms
     @PostMapping
     public Room addRoom(@RequestBody Room room) {
         return adminRoomService.addRoom(room);
     }
 
+    //adding rooms
     @PutMapping("/{id}")
     public Room updateRoom(@PathVariable String id,
                            @RequestBody Room room) {
         return adminRoomService.updateRoom(id, room);
     }
 
+    //deleting rooms
     @DeleteMapping("/{id}")
     public String deleteRoom(@PathVariable String id) {
         return adminRoomService.deleteRoom(id);
     }
 
-    // optional admin endpoints
-    @GetMapping
+    // get all rooms
+    @GetMapping("/getallRooms")
     public Object getAllRooms() {
         return adminRoomService.getAllRooms();
     }
 
+    //get rooms by id
     @GetMapping("/{id}")
     public Room getRoom(@PathVariable String id) {
         return adminRoomService.getRoom(id);
     }
 
+    //see booking details
     // 🔐 Only ADMIN can access
     @GetMapping("/bookings")
     @PreAuthorize("hasRole('ADMIN')")
@@ -52,6 +58,7 @@ public class AdminRoomController {
         return ResponseEntity.ok(adminRoomService.getAllBookingsForAdmin());
     }
 
+    //change the avilibity od the rooms
     @PatchMapping("/{roomId}/availability/{status}")
     @PreAuthorize("hasRole('ADMIN')")
     public Room setAvailability(@PathVariable String roomId,
--- src/main/java/com/elite/resort/Controller/RoomController.java
+++ src/main/java/com/elite/resort/Controller/RoomController.java
@@ -2,6 +2,7 @@ package com.elite.resort.Controller;
 
 
 import com.elite.resort.Model.Room;
+import com.elite.resort.Repository.RoomRepo;
 import com.elite.resort.Services.RoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +18,7 @@ import java.util.List;
 public class RoomController {
 
     private final RoomService roomService;
-
+    private final RoomRepo roomRepo;
     @GetMapping
     public List<Room> getAllRooms() {
         return roomService.getAllRooms();
@@ -25,6 +26,12 @@ public class RoomController {
 
     @GetMapping("/{id}")
     public Room getRoom(@PathVariable String id) {
+        Room room = roomRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Room not found"));
+
+        if (!room.isAvailable()) {
+            throw new RuntimeException("Room not available");
+        }
         return roomService.getRoomById(id);
     }
 }
