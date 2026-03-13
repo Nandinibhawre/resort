360ecdde7540c90e8bf4be53457e2b651fe4a9ab
nandini bhawre <nandinibhawre24@gmailcom>
2026-03-13 11:19:14 +0530

    added

--- src/main/java/com/elite/resort/Controller/ProfileController.java
+++ src/main/java/com/elite/resort/Controller/ProfileController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import tools.jackson.databind.ObjectMapper;
 
 import java.util.List;
 
@@ -27,10 +28,13 @@ public class ProfileController {
     @PostMapping(consumes = {"multipart/form-data"})
     public ResponseEntity<ProfileResponseDTO> createProfile(
 
-            @RequestPart("data") ProfileRequest request,
+            @RequestPart("data") String data,
             @RequestPart(value = "idProofImage", required = false) MultipartFile idProofImage,
             HttpServletRequest httpRequest
-    ){
+    ) throws Exception {
+
+        ObjectMapper mapper = new ObjectMapper();
+        ProfileRequest request = mapper.readValue(data, ProfileRequest.class);
 
         String token = httpRequest.getHeader("Authorization").substring(7);
 
